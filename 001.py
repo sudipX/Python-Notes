ab19497f4165f3b05c087833bc3b3c5203bb2db1
Sudip Poudel <107059003+sudipX@users.noreply.github.com>
2022-11-22 08:07:47 +0545

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,21 @@
+# print function -> prints the output
+print("Hello World!")
+print("Hello World!\nHello World!")
+print("Hello" + "Sudeep!")
+print("Hello" + " " + "Sudeep!")
+
+# input function -> prompts user for the input
+input("What is your name ?")
+print("Hello " + input("What is your name ?"))
+
+# len function -> prints total no. of characters in a string
+len("pauxu harayeko ma nikaliherda aankha ma timro")
+
+
+# variables -> container to store the data
+name = "paaye jaba maile timro saath jit bhayo mero savai haar"
+print(name)
+
+# capitalize function -> capitalizes first character of the string
+name = "timi bhayera artha rahyo mero saas ko"
+print(name.capitalize())
