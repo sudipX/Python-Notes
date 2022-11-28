14786911e721bfb4ef1ad4a678653e37ed9bdc02
Sudip Poudel <107059003+sudipX@users.noreply.github.com>
2022-11-28 22:24:13 +0545

    Add files via upload

---
+++ 006.py
@@ -0,0 +1,20 @@
+# Functions
+
+# defining function
+def my_function():
+    print("Hello")
+    print("Sudeep")
+
+
+# calling function
+my_function()
+
+
+# while loops
+n = 10
+while n > 0:
+    print(n)
+    n = n -1
+
+#while something_is_true:
+    #Do something repeatedly
